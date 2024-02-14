871d0ab9507547415f36d35e450d50d9fba7ccc9
Concourse <concourse@finbourne.com>
2024-02-14 19:08:34 +0000

    Updates for v2.0.52

--- sdk/src/main/java/com/finbourne/horizon/Configuration.java
+++ sdk/src/main/java/com/finbourne/horizon/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.horizon;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.50";
+    public static final String VERSION = "2.0.52";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
