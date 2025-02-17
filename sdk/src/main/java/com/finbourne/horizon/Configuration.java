ab56bc03bc4f05e2afefed2dedb64318909575fb
Concourse <concourse@finbourne.com>
2025-02-17 19:32:50 +0000

    horizon java updates for v2.0.717
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/horizon/Configuration.java
+++ sdk/src/main/java/com/finbourne/horizon/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.horizon;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.716";
+    public static final String VERSION = "2.0.717";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
