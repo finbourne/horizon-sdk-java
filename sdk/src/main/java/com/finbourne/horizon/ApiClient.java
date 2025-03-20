546f05d1667d4587b7d96f683c4eb7eea410892b
Concourse <concourse@finbourne.com>
2025-03-20 13:05:20 +0000

    horizon java updates for v2.0.816
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/horizon/ApiClient.java
+++ sdk/src/main/java/com/finbourne/horizon/ApiClient.java
@@ -212,7 +212,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/2.0.815/java");
+        setUserAgent("OpenAPI-Generator/2.0.816/java");
 
         authentications = new HashMap<String, Authentication>();
     }
