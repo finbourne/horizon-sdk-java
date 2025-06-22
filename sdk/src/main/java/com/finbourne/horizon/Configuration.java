d621186a6ab1d752e7d38eedc4361d242052b060
Concourse <concourse@finbourne.com>
2025-06-22 16:14:48 +0000

    horizon java updates for v2.0.1083
    Signed-off-by: Concourse <concourse@finbourne.com>

--- sdk/src/main/java/com/finbourne/horizon/Configuration.java
+++ sdk/src/main/java/com/finbourne/horizon/Configuration.java
@@ -12,7 +12,7 @@ package com.finbourne.horizon;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen")
 public class Configuration {
-    public static final String VERSION = "2.0.1082";
+    public static final String VERSION = "2.0.1083";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
